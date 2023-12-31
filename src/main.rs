7b0b30ebf63c70efbc52fa40335cb11705396907
sotirangelo <sotiraggelos@gmail.com>
2023-12-31 18:40:06 +0200

    feat(main): set up cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    datasources_path: String,
+    query_path: String,
+    #[arg(short, long, default_value_t = String::from("NONE"))]
+    output: String,
+    #[arg(short, long, default_value_t = String::from("ALL"))]
+    mode: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
