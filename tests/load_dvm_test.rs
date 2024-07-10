9372ae9e1a8e08364a224ff7f6f3f425a1bd32df
sotirangelo <sotiraggelos@gmail.com>
2024-07-10 22:39:18 +0300

    test(dvm_to_neo4j): create initial integration test for binary

---
+++ tests/load_dvm_test.rs
@@ -0,0 +1,75 @@
+use std::process::{self, Command};
+
+use testcontainers::{core::ContainerPort, ImageExt};
+use testcontainers_modules::{neo4j::Neo4j, testcontainers::runners::AsyncRunner};
+
+#[ignore]
+#[tokio::test]
+async fn it_loads_the_dvm() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let container = Neo4j::new()
+        .with_version("latest")
+        .with_user("neo4j")
+        .with_password("12345678")
+        .with_mapped_port(7687, ContainerPort::Tcp(7687))
+        .start()
+        .await?;
+
+    let load_dvm_output = Command::new("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg("dvm-to-neo4j")
+        .arg("--")
+        .arg("--bolt-uri")
+        .arg(format!(
+            "bolt://{}:{}",
+            container.get_host().await?,
+            container.image().bolt_port_ipv4()?
+        ))
+        .arg("test_data/example_dvm.xml")
+        .output()
+        .expect("failed to execute process");
+
+    if !load_dvm_output.status.success() {
+        eprintln!(
+            "stdout: {}",
+            String::from_utf8_lossy(&load_dvm_output.stdout)
+        );
+        eprintln!(
+            "stderr: {}",
+            String::from_utf8_lossy(&load_dvm_output.stderr)
+        );
+        process::exit(1);
+    }
+
+    // prepare neo4rs client
+    let config = neo4rs::ConfigBuilder::new()
+        .uri(format!(
+            "bolt://{}:{}",
+            container.get_host().await?,
+            container.image().bolt_port_ipv4()?
+        ))
+        .user(container.image().user().expect("user is set"))
+        .password(container.image().password().expect("password is set"))
+        .build()?;
+
+    // connect ot Neo4j
+    let graph = neo4rs::Graph::connect(config).await?;
+
+    // run a test query
+    let mut rows = graph.execute(neo4rs::query("RETURN 1 + 1")).await?;
+    while let Some(row) = rows.next().await? {
+        let result: i64 = row.get("1 + 1").unwrap();
+        assert_eq!(result, 2);
+    }
+
+    // see if the nodes are loaded
+    let mut rows = graph
+        .execute(neo4rs::query("MATCH (n) RETURN count(n)"))
+        .await?;
+    while let Some(row) = rows.next().await? {
+        let result: i64 = row.get("count(n)").unwrap();
+        assert_eq!(result, 3);
+    }
+
+    Ok(())
+}
